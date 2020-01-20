e3d304f4cf4f4d12c4181adeca0df438885db3c3
Ivan Shapovalov <intelfx@intelfx.name>
2020-01-21 02:27:52 +0300

    WIP: SpeechService: Translations.Body(): rewrite from scratch, use non-greedy right-to-left approach
    Previously, the parser would try to greedily interpret a prefix of the
    input string as a system name. Considering that both parsers were highly
    approximate, this would often misinterpret much of the input string.
    
    Rewrite the parser using a much less approximate approach and parse the
    input string right-to-left (first body name, then system name).

---
+++ Utilities/Util.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Utilities
+{
+    public class Util
+    {
+        public static List<string> FlattenMatch(Match m)
+        {
+            List<string> parts = new List<string>();
+            foreach (Group group in m.Groups)
+            {
+                foreach (Capture capture in group.Captures)
+                {
+                    var part = capture.Value.Trim();
+                    if (part != string.Empty)
+                    {
+                        parts.Add(part);
+                    }
+                }
+            }
+            parts.RemoveAt(0);
+            return parts;
+        }
+    }
+}
